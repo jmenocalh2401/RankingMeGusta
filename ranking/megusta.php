8a6acf00201dac79c7059bfee571e322b6bf5fa2
fsanchezb02 <fsanchezb02@educantabria.es>
2025-11-19 08:51:27 +0100

    Cambio Contraseña Megusta.php

--- ranking/megusta.php
+++ ranking/megusta.php
@@ -1,55 +1,48 @@
 ﻿<?php
 $dbhost = "localhost"; // Servidor
 $dbuser = "root"; // Nombre de usuario
-$dbpass = "54195419"; // Contraseña
+$dbpass = "pspspsppspspfpsepfpsfp"; // Contraseña
 $dbname = "ranking"; // Nombre de la base de datos
 
 # Creamos conexion a la base de datos
-$link = mysql_connect($dbhost,$dbuser,$dbpass);
-		mysql_select_db($dbname,$link);
+$link = mysql_connect($dbhost, $dbuser, $dbpass);
+mysql_select_db($dbname, $link);
 
 # Saber si el voto es negativo o positivo
 $voto = htmlentities($_GET['voto']);
 
 # Tomamos el id de nuestro post y vemos todas las ip que pusieron megusta
 $id = (int) $_GET['id'];
-$query = mysql_query("SELECT id,ips FROM post WHERE id='".$id."'",$link);
+$query = mysql_query("SELECT id,ips FROM post WHERE id='" . $id . "'", $link);
 $row = mysql_fetch_assoc($query);
 $ip = $row['ips'];
 
 # Obtenemos la ip de nuestro visitante		
 if ($HTTP_X_FORWARDED_FOR == "") {
-$ipp = getenv(REMOTE_ADDR);
-}
-else {
-$ipp = getenv(HTTP_X_FORWARDED_FOR);
+	$ipp = getenv(REMOTE_ADDR);
+} else {
+	$ipp = getenv(HTTP_X_FORWARDED_FOR);
 }
 
 # Me gusta o No me gusta
-switch($voto)
-{
-case "positivo";
-	if($query)
-	{
-		$var = explode(",", $ip);
-		$arr = in_array($ipp, $var);
-		if(!$arr)
-		{
-			mysql_query("UPDATE post SET megusta=megusta+1, ips=CONCAT(post.ips,'".$ipp.",') WHERE id='".$id."'",$link);
+switch ($voto) {
+	case "positivo";
+		if ($query) {
+			$var = explode(",", $ip);
+			$arr = in_array($ipp, $var);
+			if (!$arr) {
+				mysql_query("UPDATE post SET megusta=megusta+1, ips=CONCAT(post.ips,'" . $ipp . ",') WHERE id='" . $id . "'", $link);
+			}
 		}
-	}
-break;	
-case "negativo";
-	if($query)
-	{
-		$var = explode(",", $ip);
-		$arr = in_array($ipp, $var);
-		if(!$arr)
-		{
-			mysql_query("UPDATE post SET megusta=megusta-1, ips=CONCAT(post.ips,'".$ipp.",') WHERE id='".$id."'",$link);
+		break;
+	case "negativo";
+		if ($query) {
+			$var = explode(",", $ip);
+			$arr = in_array($ipp, $var);
+			if (!$arr) {
+				mysql_query("UPDATE post SET megusta=megusta-1, ips=CONCAT(post.ips,'" . $ipp . ",') WHERE id='" . $id . "'", $link);
+			}
 		}
-	}
-break;
+		break;
 }
 header('location:index.php');
-?>
